35e4c7ff8f7a65cc2e7bd9747457a08ddbfa2ed9
Abdelrhman <42518131+abdelrhman28@users.noreply.github.com>
2024-05-17 23:37:40 +0300

    Update BaseTest.java

--- BaseTest.java
+++ BaseTest.java
@@ -2,6 +2,9 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.*;
 
@@ -20,35 +23,45 @@ public class BaseTest {
     // WebDriverWait instance for waiting for elements
 
     WebDriverWait wait = new WebDriverWait(drv1, 10);
+    @BeforeTest
 
+    public void BaseTest(){
+        drv1 = DriverManager.getDriver();
+    }
 
     @Test
     public void Tester1() {
-        //DriverManager.getDriver();
         testLoginPage.loginPage("standard_user","secret_sauce", drv1, wait);
         testProduct.ProductPage(drv1, wait);
         testCart.CartPage(drv1, wait);
         testCheckOut.CheckOutPage(drv1, wait);
         testOverView.OverViewPage(drv1, wait);
-        //drv1.switchTo();
+
     }
 
+    @BeforeMethod
+    public void Link(){
 
+        drv1.get(DriverManager.BROWSER_LINK);
+    }
 
     @Test
     public void Tester2() {
-        //DriverManager.getDriver();
-        drv1.get(DriverManager.BROWSER_LINK);
         System.out.println(drv1.getCurrentUrl());
         testLoginPage.loginPage("visual_user", "secret_sauce", drv1, wait);
         testProduct.ProductPage(drv1, wait);
         testCart.CartPage(drv1, wait);
         testCheckOut.CheckOutPage(drv1, wait);
         testOverView.OverViewPage(drv1, wait);
-        drv1.quit();
+
 
         DriverManager.quitDriver();
         // Close WebDriver instance after test
+    }
+    @AfterTest
+    public void quitDriver(){
+        drv1.quit();
+
     }
 
     // Ensure that WebDriver instances are properly closed after each test method
